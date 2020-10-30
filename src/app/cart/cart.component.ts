ce270f0882ffbe32498306baf14c35ef327fcfa6
mlntdrv <mlntdrv@gmail.com>
2020-10-30 12:05:39 +0200

    1 file modified, 1 added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,5 +12,7 @@ export class CartComponent implements OnInit {
 
   constructor(private cartService: CartService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
 }
